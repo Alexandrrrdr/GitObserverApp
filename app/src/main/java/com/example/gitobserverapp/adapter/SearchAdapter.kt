864d5ef299709cb8d816bd0d6a7ee2112619757c
maximlozhechnikov <98010383+maximlozhechnikov@users.noreply.github.com>
2022-12-06 18:30:36 +0300

    recycler view some base realization was added

--- app/src/main/java/com/example/gitobserverapp/adapter/SearchAdapter.kt
+++ app/src/main/java/com/example/gitobserverapp/adapter/SearchAdapter.kt
@@ -1,29 +1,29 @@
 package com.example.gitobserverapp.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gitobserverapp.databinding.RecItemBinding
+import com.example.gitobserverapp.response.RepoListExample
 
-class SearchAdapter: RecyclerView.Adapter<SearchAdapter.SearchHolder>() {
+class SearchAdapter(private val repoList: List<RepoListExample>): RecyclerView.Adapter<SearchAdapter.SearchHolder>() {
 
-    private lateinit var binding: RecItemBinding
-
-    inner class SearchHolder(view: View): RecyclerView.ViewHolder(view) {
-        fun bind(){
-
+    inner class SearchHolder(view: View): RecyclerView.ViewHolder(view){
+        fun bind(repoItem: RepoListExample){
+            //TODO after will be created Model for response
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchHolder {
-        TODO("Not yet implemented")
+        val binding = RecItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return SearchHolder(binding.root)
     }
 
     override fun onBindViewHolder(holder: SearchHolder, position: Int) {
-        TODO("Not yet implemented")
+        val repoItem = repoList[position]
+        holder.bind(repoItem = repoItem)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = repoList.size
 }
---
+++ app/src/main/java/com/example/gitobserverapp/response/RepoListExample.kt
@@ -0,0 +1,4 @@
+package com.example.gitobserverapp.response
+
+class RepoListExample {
+}
