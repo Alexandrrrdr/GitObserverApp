985e7d01974a74edf6c5ef610c4799f4277e5404
Alexandrrrdr <albakhtinag@gmail.com>
2023-02-15 16:00:17 +0300

    Init bar char logic was excluded from view. App is working properly.

--- app/src/main/java/com/example/gitobserverapp/ui/screens/barchart/ChartFragment.kt
+++ app/src/main/java/com/example/gitobserverapp/ui/screens/barchart/ChartFragment.kt
@@ -1,36 +1,22 @@
 package com.example.gitobserverapp.ui.screens.barchart
 
 import android.content.Context
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
-import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.gitobserverapp.App
+import com.example.gitobserverapp.R
 import com.example.gitobserverapp.databinding.FragmentChartBinding
 import com.example.gitobserverapp.domain.usecase.GetStarGroupUseCase
 import com.example.gitobserverapp.ui.screens.barchart.model.BarChartModel
-import com.example.gitobserverapp.ui.screens.barchart.model.UiStarGroup
 import com.example.gitobserverapp.ui.screens.details.User
 import com.example.gitobserverapp.utils.Constants.START_PAGE
 import com.example.gitobserverapp.utils.mapper.UiStarGroupMapper
-import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.components.YAxis
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
-import com.github.mikephil.charting.formatter.ValueFormatter
-import com.github.mikephil.charting.highlight.Highlight
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener
-import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.android.material.snackbar.Snackbar
 import moxy.MvpAppCompatFragment
 import moxy.presenter.InjectPresenter
@@ -39,11 +25,12 @@ import javax.inject.Inject
 
 
 class ChartFragment():
-    MvpAppCompatFragment(), ChartView {
+    MvpAppCompatFragment(), ChartView, BarChartCreate.Listener {
 
     @Inject lateinit var getStarGroupUseCase: GetStarGroupUseCase
     @Inject lateinit var uiStarGroupMapper: UiStarGroupMapper
 
+    private lateinit var barChartCreate: BarChartCreate
     @InjectPresenter
     lateinit var chartViewPresenter: ChartViewPresenter
 
@@ -62,13 +49,6 @@ class ChartFragment():
     private var page = 1
     private var lastPage = 1
 
-    //Start creating barCharts
-    private lateinit var barDataSet: BarDataSet
-    private lateinit var barchartGraph: BarChart
-    private var barEntryList = mutableListOf<BarEntry>()
-    private var barLabelList = mutableListOf<String>()
-
-
     override fun onAttach(context: Context) {
         super.onAttach(context)
         (requireContext().applicationContext as App).appComponent.inject(this@ChartFragment)
@@ -86,6 +66,7 @@ class ChartFragment():
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        barChartCreate = BarChartCreate(binding.barChart, this)
         repoName = args.repoName
         repoOwnerName = args.repoOwnerName
         binding.repoName.text = repoName
@@ -143,7 +124,7 @@ class ChartFragment():
         this.lastPage = lastPage
         this.page = page
         navigationButtonsController(lastPage = lastPage, page = page)
-        initBarChart(list = list)
+        barChartCreate.initBarChart(list = list)
     }
 
     override fun showErrorPage(error: String) {
@@ -159,122 +140,23 @@ class ChartFragment():
         binding.progBarChart.visibility = View.GONE
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun initBarChart(list: List<BarChartModel>) {
-
-        barchartGraph = binding.barChart
-        createBarChartData(list)
-        barDataSet = BarDataSet(barEntryList.reversed(), "")
-        val barData = BarData(barDataSet)
-
-        //Hide unnecessary labels if no data in AXis
-        barData.setValueFormatter(object : ValueFormatter() {
-            override fun getFormattedValue(value: Float): String {
-                return if (value > 0) {
-                    super.getFormattedValue(value)
-                } else {
-                    ""
-                }
-            }
-        })
-
-        barchartGraph.data = barData
-        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS, 250)
-        barDataSet.valueTextSize = 14f
-        barDataSet.valueTextColor = Color.BLACK
-        barchartGraph.description.isEnabled = false
-        barchartGraph.axisRight.isEnabled = false
-        barchartGraph.isDragEnabled = false
-
-        barchartGraph.setVisibleXRangeMaximum(5f)
-        barchartGraph.setVisibleXRangeMinimum(5f)
-        barchartGraph.animateY(1000)
-        barchartGraph.animateX(1000)
-
-        val xAxis: XAxis = barchartGraph.xAxis
-
-        //set labels
-        xAxis.valueFormatter = IndexAxisValueFormatter(barLabelList.reversed())
-        xAxis.setCenterAxisLabels(false)
-        xAxis.setDrawGridLines(false)
-        xAxis.granularity = 1f
-        xAxis.isGranularityEnabled = true
-        xAxis.position = XAxis.XAxisPosition.BOTTOM
-        xAxis.textColor = Color.BLACK
-        xAxis.textSize = 14f
-
-        val axisLeft: YAxis = barchartGraph.axisLeft
-        axisLeft.granularity = 1f
-        axisLeft.axisMinimum = 0f
-
-        barchartGraph.setOnChartValueSelectedListener(object : OnChartValueSelectedListener {
-            override fun onValueSelected(e: Entry?, h: Highlight?) {
-                //getting index of bar is selected
-                val x = e!!.x.toInt()
-                val year = barLabelList[x]
-                val userList: Any? = barEntryList[x].data
-
-                //Check List is List and same type and send safeArgs and Open new Fragment
-                if (userList is List<*>) {
-                    getViaPoints(userList)?.let { list ->
-                        if (list.isNotEmpty()) {
-                            val tmp: List<User> = list.map { User(
-                                id = it.users.id,
-                                login = it.users.name,
-                                avatar_url = it.users.userAvaUrl)
-                            }
-                            val arrayList: Array<User> = tmp.toTypedArray()
-                            val direction =
-                                ChartFragmentDirections.actionChartFragmentToDetailsFragment(
-                                    timePeriod = year,
-                                    amountUsers = userList.size,
-                                    list = arrayList
-                                )
-                            findNavController().navigate(directions = direction)
-                        }
-                    }
-                }
-            }
-
-            override fun onNothingSelected() {
-                Snackbar.make(binding.root, "Field is Empty", Snackbar.LENGTH_SHORT).show()
-            }
-        })
-    }
-
-    //Check BarChart data.object after click on line
-    private fun getViaPoints(list: List<*>): List<UiStarGroup>? {
-        list.forEach { if (it !is UiStarGroup) return null }
-        return list.filterIsInstance<UiStarGroup>()
-            .apply { if (size != list.size) return null }
-    }
-
-    private fun createBarChartData(list: List<BarChartModel>) {
-        barEntryList.clear()
-        barLabelList.clear()
-        for (i in list.indices) {
-            barLabelList.add(i, list[i].period.toString())
-            if (list[i].userInfo.isEmpty()){
-                barEntryList.add(
-                    BarEntry(
-                        i.toFloat(),
-                        list[i].userInfo.size.toFloat()
-                    )
-                )
-            } else {
-                barEntryList.add(
-                    BarEntry(
-                        i.toFloat(),
-                        list[i].userInfo.size.toFloat(),
-                        list[i].userInfo
-                    )
-                )
-            }
-        }
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    override fun click(item: List<User>, year: String) {
+        val arrayList: Array<User> = item.toTypedArray()
+        val direction =
+            ChartFragmentDirections.actionChartFragmentToDetailsFragment(
+                timePeriod = year,
+                amountUsers = arrayList.size,
+                list = arrayList
+            )
+        findNavController().navigate(directions = direction)
+    }
+
+    override fun nothingClick() {
+        Snackbar.make(binding.root, requireContext().getText(R.string.nothing_selected), Snackbar.LENGTH_SHORT).show()
     }
 }
